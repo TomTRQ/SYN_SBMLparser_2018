41ac9ae6fd76bee8ceffb021f81a745454566b98
TomTRQ <50371010+TomTRQ@users.noreply.github.com>
2019-06-10 09:47:19 +0200

    Add files via upload
    FIRST PUSH
    ADD requirement.c

---
+++ requirement.c
@@ -0,0 +1,63 @@
+/*
+** EPITECH PROJECT, 2019
+** ON SYNTHESE PAS NOUS
+** File description:
+** Requirement.c
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+static int find_cut_char(char c)
+{
+    if (c < 48 || (c > 57 && c < 65) || (c > 90 && c < 97) || c > 122)
+        return (1);
+    else
+        return (0);
+}
+
+static int spliter_count(char const *tab)
+{
+    int i = 0;
+    int words = 0;
+
+    while (tab[i] != '\0') {
+        words += (find_cut_char(tab[i]) == 0 && tab[i] != '\0');
+        for (; tab[i] != '\0' && find_cut_char(tab[i]) == 0; i++);
+        i++;
+    }
+    return (words);
+}
+
+char **my_str_to_word_array_synthesis(char const *str)
+{
+    char **args = NULL;
+    int size = spliter_count(str);
+    int i = 0;
+    int k = 0;
+
+    args = malloc(sizeof(char *) * (size + 1));
+    for (int j = 0; j < size; j++) {
+        for (k = 0 ; find_cut_char(str[i + k]) == 0 && \
+        str[i + k] != '\0' ; k++);
+        args[j] = malloc(sizeof(char) * (k + 1));
+        for (int l = 0; l < k; l++, i++)
+            args[j][l] = str[i];
+        args[j][k] = '\0';
+        i++;
+    }
+    args[size] = NULL;
+    return (args);
+}
+
+/*int main(int argc, char **argv)
+{
+    unsigned char test[23] = "BONJOUR-BONSOIR_NON%ah\0";
+    char **hey = my_str_to_word_array_synthesis(test);
+
+    for (int i = 0; hey[i] != NULL; ++i) {
+        printf("%s\n", hey[i]);
+        if (hey[i] == NULL)
+            exit (1);
+    }
+}*/
